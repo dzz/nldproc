8e3a56e15b93291cba75f6ccf3a1bed07d778a5e
devon <devon.zachary@gmail.com>
2017-03-26 21:25:34 -0700

    scratch for ...you know

--- src/scratch.cpp
+++ src/scratch.cpp
@@ -1,3 +1,31 @@
+//Sample interface for buffer routing code
+//
+//
+//   Building the following DSP topology
+//
+//   () indicate buffer id
+//
+//            
+//
+//       [ audio input(0) ]
+//                   |
+//                  / \
+//                 /   \
+//          [ LPF (1) ] [ peakfollower (2) ]
+//                |    /
+//                |   /
+//                |  /
+//                | /
+//             [ multiply (1) ]
+//                |
+//                |
+//             [ waveshaping distortion (1) ]
+//                |
+//                |
+//             [ audio output(0) ]
+//         
+//
+//
 
 
 void plugin::plugin() {
@@ -12,24 +40,54 @@ void plugin::plugin() {
     //the chunksize based on the audio drivers requested buffer size
     this->buffers.set_chunksize(1024);
 
-    //create two inputs which will be mapped to pointers
+    //create an input which will be mapped to the audio 
+    //audio driver's requested memory location
     this->buffers.create_ptrinput_buffer("input");
-    this->buffers.create_ptrinput_buffer("output");
 
+
+    //create the LPF buffer
     this->buffers.create_memory_buffer("lpf");
+
+    //create the Peakfollower buffer
+    this->buffers.create_memory_buffer("peakfollower");
+
+    //create alias buffers for semantic naming, reusing the same
+    //buffer to process in place
     this->buffers.create_alias_buffer_from_to("lpf", "lpf*peakfollower");
     this->buffers.create_alias_buffer_from_to("lpf", "lpf*peakfollower (distorted)");
-    this->buffers.create_memory_buffer("peakfollower");
+
+    //create an alias buffer from output to input, as the audio driver
+    //wants results in the same memory location requested in 'process'
+    this->buffers.create_alisa_buffer_from_to("input", "output");
+
 }
 
 void plugin::process(double* channels) {
 
+    //arm the input buffer, which also arms output
     this->buffers.arm_ptrinput_buffer("input", channels );
-    this->buffers.arm_ptrinput_buffer("output", channels );
-    this->buffers.process_to_with_from("lpf", lpf_filter,  "input" );
-    this->buffers.process_to_with_from("peakfollower", peakfollower, "input" );
+
+    //process "input" with the LPF putting results in "lpf"
+    this->buffers.process_to_with_from("lpf",           this->lpf_filter,   "input" );
+
+    //process "input" with the peakfollower, putting results in "peakfollower"
+    this->buffers.process_to_with_from("peakfollower",  this->peakfollower, "input" );
+
+    //multiply "peakfollower" by "lpf*peakfollower" putting results in "lpf*peakfollower",
+    //
+    // -- notice that we're reusing the "lpf" buffer here because it's an alias and we
+    //    don't need to preserve the values from the "lpf" buffer
     this->buffers.multiply_right("peakfollower", "lpf*peakfollower");
+
+    //process "lpf*peakfollower (distorted)" in place with the waveshaper
+    //
+    // -- again, we're really using the "lpf*peakfollower" buffer which itself
+    //    is just the "lpf" buffer
     this->buffers.process_in_place_with("lpf*peakfollower (distorted)", waveshaper );
+
+    //copy the memory in "lpf*peakfollower (distorted)" into output, which is
+    //an alias of "output" which is the double* chanels pointer passed into this
+    //function
     this->buffers.clone_from_to("lpf*peakfollower (distorted)","output");
 
 }
