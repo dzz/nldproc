53628595be730c3b4d19929a8ba12912fc3cf796
devon <devon.zachary@gmail.com>
2017-04-14 17:36:28 -0700

    verify delay

--- src/tests/test_delay.h
+++ src/tests/test_delay.h
@@ -6,11 +6,13 @@ int test_delay() {
     stereo_buffer   master_buffer = test_pipe.create_unmapped_buffer(); 
     delay       test_delay(128);
 
-    file::unsafe_fill_from_file(  NLDPROC_TEST_RAW_AUDIO_FILE, master_buffer );
     test_pipe.assign_ptr_buffer( {"buf:master"}, master_buffer );
+
+    sine::fill_range( 0.25, test_pipe.get_mapped_buffer( "buf:master" ), 0, 255 ); 
+    test::write_input_signal_test_data( &test_pipe, "buf:master");
+
     test_pipe.map_processor( &test_delay, {"proc:delay"} );
 
-    test::write_input_signal_test_data( &test_pipe, "buf:master");
     test_pipe.process_with_inplace( "proc:delay", "buf:master" );
 
     // write test report info to file 
